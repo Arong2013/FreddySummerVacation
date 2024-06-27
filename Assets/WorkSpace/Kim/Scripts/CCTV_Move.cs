8aaf2bfc9a82f8338136c7fffce2300fda299ec1
kimgyungho <sgum125@naver.com>
2024-06-27 20:00:34 +0900

    상호작용, cctv 생성

--- Assets/WorkSpace/Kim/Scripts/CCTV_Move.cs
+++ Assets/WorkSpace/Kim/Scripts/CCTV_Move.cs
@@ -1,20 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour
+public class CCTV_Move : MonoBehaviour
 {
-    bool lockRotate = true;
+    /*자동으로 움직임
+    public float rotationSpeed = 10f;
+    private float rotationY;
+    */
+    bool lockRotate = false;//직접 움직임
     public float mouseSensitivity = 100.0f; // 마우스 감도
     private float xRotation = 0.0f; // x축 회전 값
     private float yRotation = 0.0f; // y축 회전 값
-
     public bool Lock { set {lockRotate = value;} }
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked; // 마우스 커서를 화면 중앙에 고정
-    }
-
     void Update()
     {
+        /*rotationY += rotationSpeed * Time.deltaTime; //자동
+        transform.localRotation = Quaternion.Euler(0, rotationY, 0);*/
+
         if(lockRotate)
         {
             float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;

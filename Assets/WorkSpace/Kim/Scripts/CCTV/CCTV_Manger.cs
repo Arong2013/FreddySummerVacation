956f9db41feae494c24c0ac0ceb719f5b7959702
kimgyungho <sgum125@naver.com>
2024-10-06 00:58:51 +0900

    시간, cctv 배터리

--- Assets/WorkSpace/Kim/Scripts/CCTV/CCTV_Manger.cs
+++ Assets/WorkSpace/Kim/Scripts/CCTV/CCTV_Manger.cs
@@ -7,6 +7,8 @@ using UnityEngine.InputSystem;
 using UnityEngine.Assertions.Must;
 using UnityEngine.Analytics;
 using UnityEngine.Audio;
+using TMPro;
+using UnityEditor.ShaderKeywordFilter;
 
 public enum CCTV_POS
 {
@@ -30,19 +32,32 @@ public class CCTV_Manger : Singleton<CCTV_Manger>
     [SerializeField] AudioClip closeCCTV_Clip;
     [SerializeField] AudioClip cctv_bgm;//cctv를 볼 동안 나올 bgm
     [SerializeField] AudioClip origin_bgm;//원래 bgm
+    [SerializeField] TextMeshProUGUI cctv_Battery_Text;
     public GameObject Get_CCTV_Select {get { return CCTV_Select;}}
     public bool IsOn_CCTV { set{ isOn_CCTV = value; } get { return isOn_CCTV; }}
     [SerializeField] Camera cur_cam;
-    [SerializeField] int cctv_battery; 
+    [SerializeField] float max_cctv_battery; 
+    [SerializeField] float cctv_battery_watching_decrease_delay;//cctv를 보고 있을때 줄어드는 속도
+    [SerializeField] float cctv_battery_decrease_delay;//cctv배터리가 줄어드는 속도
+    [SerializeField] bool is_cctv_battery_down;//cctv배터리가 깍이고 있는지
+    public bool Is_cctv_battery_down { set{ is_cctv_battery_down = value; } get { return is_cctv_battery_down; }}
     bool isBroken = false;
     bool isOn_CCTV = false;
-    Coroutine cctv_battery_down;
+    float cur_cctv_battery;
+    float cur_cctv_battery_decrease_delay;
+    Coroutine cctv_battery_down_Coroutine;
     public bool IsBroken { set{ isBroken = value; noise_image.gameObject.SetActive(value); } get { return isBroken; } }
     public void Initialize()
     {
         isBroken = false;
         isOn_CCTV = false;
-        cctv_battery = 100;
+        cur_cctv_battery = max_cctv_battery;
+        cctv_battery_down_Coroutine = StartCoroutine(CCTV_Battery_Down());
+    }
+    public void GameEnd()
+    {
+        StopCoroutine(cctv_battery_down_Coroutine);
+        Turn_Off_CCTV();
     }
     public void Set_CCTV_Screen(CCTV_POS room_name)//어떤 cctv를 볼건지 선택후 실행
     {
@@ -64,10 +79,15 @@ public class CCTV_Manger : Singleton<CCTV_Manger>
             cctv_view.texture = cur_cam.targetTexture;
             cctv_view.gameObject.SetActive(true);
         }
-        cctv_battery_down = StartCoroutine(CCTV_Battery_Down());
         Sound_Manager.Instance.PlayBGM(cctv_bgm);
+        DecreaseBattery();
+        cur_cctv_battery_decrease_delay = cctv_battery_watching_decrease_delay;
     }
     public void Turn_Off_CCTV(InputAction.CallbackContext callbackContext)
+    {
+        Turn_Off_CCTV();
+    }
+    public void Turn_Off_CCTV()
     {
         if(Game_Manager.Instance.GetPlayer.IsStop && isOn_CCTV)//플레이어가 멈춰있을때/게임이 정지된 상태일 때만
         {
@@ -81,19 +101,31 @@ public class CCTV_Manger : Singleton<CCTV_Manger>
             Game_Manager.Instance.GetPlayer.IsStop = false;
             Sound_Manager.Instance.PlaySFX(closeCCTV_Clip, (int)SFX_SOURCE_INDEX.NORMAL_SFX);
             Sound_Manager.Instance.PlayBGM(origin_bgm);
+            cur_cctv_battery_decrease_delay = cctv_battery_decrease_delay;
         }
     }
-    IEnumerator CCTV_Battery_Down()
+    public IEnumerator CCTV_Battery_Down()
     {
-        yield return new WaitForSeconds(1f);
-        cctv_battery--;
-        if(cctv_battery <= 0)
+        while(is_cctv_battery_down)
         {
-            CCTV_Down();
+            yield return new WaitForSeconds(cctv_battery_decrease_delay);
+            DecreaseBattery();
+            if(cur_cctv_battery <= 0)
+            {
+                CCTV_Down();
+            }
         }
     }
     void CCTV_Down()
     {
         //cctv배터리 다 됬을때
+    }
+    public void DecreaseBattery(float decreaseAmount = 1)
+    {
+        cur_cctv_battery -= decreaseAmount;
+        if(cur_cctv_battery <= 0)
+            cctv_Battery_Text.text = "0";
+        else
+            cctv_Battery_Text.text = $"{cur_cctv_battery}%";//Interpolated Strings
     }
 }
--- Assets/WorkSpace/Kim/Scripts/Game_Manager.cs
+++ Assets/WorkSpace/Kim/Scripts/Game_Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
@@ -32,12 +33,17 @@ public class Game_Manager : Singleton<Game_Manager>
     [SerializeField] PlayerInput input;
     [SerializeField] Door door;
     [SerializeField] Note note;
+    [SerializeField] TextMeshProUGUI time_text;
+    [SerializeField] float time_text_move_duration;//시간 텍스트 이동하는데 걸리는 시간
+    [SerializeField] int day = 1;
+    [SerializeField] float time_delay;//1시간 지나는데 걸리는 시간 초단위
+    [SerializeField] int cur_time = 0;
     [SerializeField] bool villainTest_A;
     [SerializeField] bool villainTest_B;
     [SerializeField] bool villainTest_C;
     [SerializeField] bool villainTest_D;
     [SerializeField] bool villainTest_E;
-    int day = 1;
+    Coroutine time_Coroutine;
     public Player GetPlayer => player;
     void Start()
     {
@@ -62,11 +68,56 @@ public class Game_Manager : Singleton<Game_Manager>
             Villain_Manager.Instance.StartMove(VILLAIN_INDEX.D);
         if(villainTest_E)
             Villain_Manager.Instance.StartMove(VILLAIN_INDEX.E);
-
-
-
+        
+        GameStart();
+    }
+    public void GameStart()
+    {
         player.Initialize();
         CCTV_Manger.Instance.Initialize();
-        //Villain_Manager.Instance.villain_Cycle(day);
+        Villain_Manager.Instance.villain_Cycle(day);
+        //StartCoroutine(TextMove());
+        time_Coroutine = StartCoroutine(UpdateTime());
+    }
+    public IEnumerator GameEnd()
+    {
+        CCTV_Manger.Instance.GameEnd();
+        Villain_Manager.Instance.GameEnd();
+        Sound_Manager.Instance.StopBGM();
+        StopCoroutine(time_Coroutine);
+        yield return new WaitForSeconds(0);//밤씬 끝날때 연출 넣을 용도
+        //낮 씬으로 넘기기
+    }
+    IEnumerator UpdateTime()
+    {
+        while(true)
+        {
+            time_text.text = $"{cur_time} : 00";//Interpolated Strings
+            yield return new WaitForSeconds(time_delay);
+            cur_time++;
+            if(cur_time >= 6)//6시가 되면 밤씬 끝
+            {
+                time_text.text = $"{cur_time} : 00";
+                day++;
+                //StartCoroutine(GameEnd());
+            }
+        }
+    }
+    IEnumerator TextMove()
+    {   
+        Vector2 targetPos = time_text.rectTransform.anchoredPosition;//텍스트가 갈 위치
+        time_text.rectTransform.anchoredPosition = new Vector2(0, 0);//텍스트를 화면 중앙으로 이동
+        float elapsedTime = 1;
+        while (elapsedTime < time_text_move_duration)
+        {
+            // 경과 시간에 비례하여 현재 위치 계산
+            float t = elapsedTime / time_text_move_duration;
+            time_text.rectTransform.anchoredPosition = Vector2.Lerp(new Vector2(0, 0), targetPos, t);
+            elapsedTime += Time.deltaTime;
+            yield return null; // 다음 프레임까지 대기
+        }
+
+        // 최종 목표 위치로 설정
+        time_text.rectTransform.anchoredPosition = targetPos;
     }
 }
